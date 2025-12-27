33df5866671fc408d80293a40d6e235a8ef4e0a6
maria <informatica1bachIES@gmail.com>
2025-12-27 20:15:33 +0000

    escena por defecto

--- python_connect.gd
+++ python_connect.gd
@@ -1,19 +1,19 @@
 extends Node
-
+ 
 var server := UDPServer.new()
 var json := JSON.new()
-
+ 
 func _ready():
 	var ok = server.listen(6005)
 	if ok != OK:
 		print("Error al abrir el puerto UDP")
 	else:
 		print("Godot escuchando en puerto 6005...")
-
+ 
 func _process(delta):
 	# Aceptar nuevos emisores de datos
 	server.poll()
-
+ 
 	# Obtener una lista de todas las conexiones UDP
 	while server.is_connection_available():
 		var peer = server.take_connection()
@@ -21,21 +21,23 @@ func _process(delta):
 		var parsed = json.parse(data)
 		if parsed == OK:
 			var json_data = json.data
-			if json_data["move_x"] != null:
-				var value = json_data["move_x"]
-				_move_x(value[0], value[1])
+			if json_data["move_right"] == 1:
+				_move_x("derecha")
+			elif json_data["move_left"] == 1:
+				_move_x("izquierda")
 			elif json_data["jump"] == 1:
 				_jump()
 			elif json_data["shoot"] == 1:
 				_shoot()
-			
-			
+			elif json_data["crouch"] == 1:
+				_crouch()
 
-func _move_x(direction, blocks):
-	print("Moviendose hacia la ", direction, " ", blocks, " bloques")
-	
+ 
+func _move_x(direction):
+	print("Moviendose hacia la ", direction)
+func _crouch():
+	print("Crouch")
 func _jump():
 	print("Jump!")
-	
 func _shoot():
 	print("Shoot")
