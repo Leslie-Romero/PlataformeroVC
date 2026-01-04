33a14028b59037d4f932c92883db12b4f954999d
Leslie Romero <ll.rommar@gmail.com>
2026-01-04 13:32:07 +0000

    Arreglado el ghost jumping

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,7 @@ const gravity = 15
 
 @onready var sprite = $Sprite2D
 @onready var animationPlayer = $AnimationPlayer 
+var jump_cooldown = 0.0
 
 func _ready():
 	if Global.checkpoint_pos != null:
@@ -15,6 +16,13 @@ func _ready():
 func _physics_process(delta):
 	velocity.y += gravity
 	var friction = false
+	
+	if jump_cooldown > 0:
+		jump_cooldown -= delta
+		
+	if not is_on_floor() or jump_cooldown > 0:
+		if Global.jump != 0:
+			Global.jump = 0
 	
 	if Global.slide == 1:
 		animationPlayer.stop()
@@ -40,17 +48,22 @@ func _physics_process(delta):
 			animationPlayer.play("Idle")
 		friction = true
 	
+	
+	
 	if is_on_floor():
-		if Global.jump == 1: 
-			sprite.flip_h = true 
-			velocity.y = jumpHeight
-			velocity.x = maxSpeed * 1.5
-			Global.jump = 0
-		elif Global.jump == 2:
-			sprite.flip_h = false
-			velocity.y = jumpHeight
-			velocity.x = -maxSpeed * 1.5
-			Global.jump = 0
+		if jump_cooldown <= 0:
+			if Global.jump == 1: 
+				sprite.flip_h = true 
+				velocity.y = jumpHeight
+				velocity.x = maxSpeed * 1.5
+				Global.jump = 0
+				jump_cooldown = 0.2
+			elif Global.jump == 2:
+				sprite.flip_h = false
+				velocity.y = jumpHeight
+				velocity.x = -maxSpeed * 1.5
+				Global.jump = 0
+				jump_cooldown = 0.2
 			
 		if friction:
 			velocity.x = lerp(velocity.x, 0.0, 0.2) 
