811b2421f02819d7f49537f04853b054aaaa0ba2
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-01-04 13:24:58 +0000

    el checkpoint cambia cuando lo coges

--- Scenes/checkpoint.gd
+++ Scenes/checkpoint.gd
@@ -1,6 +1,15 @@
 extends Area2D
 
+func _ready():
+	if Global.checkpoint_pos == global_position:
+		modulate = Color(0, 1, 0)
+
 func _on_body_entered(body):
 	if body.name == "Player":
-		Global.checkpoint_pos = global_position
-		print("Checkpoint guardado en: ", global_position)
+		if Global.checkpoint_pos != global_position:
+			Global.checkpoint_pos = global_position
+			
+			var tween = create_tween()
+			tween.tween_property(self, "modulate", Color(0.3, 1, 0.3), 0.2)
+			tween.parallel().tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)
+			tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
